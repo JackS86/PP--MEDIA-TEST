00e62937c4805acd2678520da31e384052212b6e
Jacek Stępień <jacekstepien6@gmail.com>
2021-11-26 20:27:38 +0100

    next part update css and index

--- functionaly.js
+++ functionaly.js
@@ -10,8 +10,8 @@ let timeline = new TimelineMax();
 
 timeline
     .to(".title-one", 3, { y: -240 })
-    .to(".title-main", 3, { y: 460 }, "-=3")
-    .to(".title-two", 3, { y: 450 }, "-=3")
+    .to(".title-main", 3, { y: 360 }, "-=3")
+    .to(".title-two", 3, { y: 480 }, "-=3")
     .to("#experience", 3, { top: "50%" }, "-=3");
 
 let scene = new ScrollMagic.Scene({
